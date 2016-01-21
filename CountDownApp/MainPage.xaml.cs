71ec2cc2072a4a80fbcc50f3f818510d15031bb7
UnderDev <scottcoyne@hotmail.com>
2016-01-21 00:24:35 +0000

    Template

---
+++ CountDownApp/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CountDownApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnSinglePlayer_Click(object sender, RoutedEventArgs e)
+        {
+            //Navigate to the Main Game Page
+            Frame.Navigate(typeof(gamePage));
+        }
+
+        private void btnMultiPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            //Navigate to the Main Game Page
+            Frame.Navigate(typeof(gamePage));
+        }
+
+        private void btnRules_Click(object sender, RoutedEventArgs e)
+        {
+            //Navigate to the Main Rules Page
+            Frame.Navigate(typeof(rulesPage));
+        }
+    }
+}
---
+++ CountDownApp/gamePage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CountDownApp
+{
+
+    public sealed partial class gamePage : Page
+    {
+
+        protected char[] _vowlesArray = new char[5] { 'A', 'E', 'I', 'O', 'U'};
+        protected char[] _consonantsArray = new char[21] { 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R' , 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'};
+
+        private int rndNum = 0;
+
+        public gamePage()
+        {
+            this.InitializeComponent();
+
+
+        }
+
+        private void btnVowels_Click(object sender, RoutedEventArgs e)
+        {
+            rndNum = rndNumGenerator(_vowlesArray.Length);
+        }
+
+        private void btnConsonants_Click(object sender, RoutedEventArgs e)
+        {
+            rndNum = rndNumGenerator(_consonantsArray.Length);
+        }
+
+
+        //Generates a number between 0 and the number passed in
+        private int rndNumGenerator(int maxNo)
+        {
+            var r = new Random();
+            int   num = r.Next(maxNo);
+            return num;
+        }
+
+
+
+    }
+}
