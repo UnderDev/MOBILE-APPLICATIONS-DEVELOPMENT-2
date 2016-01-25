488dc8a68166f9231be858490448adbf36ee1c3a
UnderDev <scottcoyne@hotmail.com>
2016-01-25 22:31:08 +0000

    Added New Feature: * Checks the users input for valid letters and makes sure the user cant Re-Use letters

--- CountDownApp/gamePage.xaml.cs
+++ CountDownApp/gamePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -13,8 +14,9 @@ namespace CountDownApp
 
         private char[] _vowlesArray = new char[5] { 'A', 'E', 'I', 'O', 'U'};
         private char[] _consonantsArray = new char[21] { 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R' , 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'};
+        private List<Char> _listAvailLetters = new List<char>();
 
-        TextBox[] _textboxeArray = new TextBox [9];
+        TextBox[] _textboxArray = new TextBox [9];
 
         private int _rndNum = 0 , _textBoxLoc = 0;
 
@@ -30,9 +32,9 @@ namespace CountDownApp
         //gets the amount of textboxes from gamePage and puts them into a list
         private void getTextBoxList()
         {
-            for (int i = 0; i < _textboxeArray.Length; i++)
+            for (int i = 0; i < _textboxArray.Length; i++)
             {
-                _textboxeArray[i] = ((TextBox)LettersGrid.FindName("textBox" + i.ToString()));
+                _textboxArray[i] = ((TextBox)LettersGrid.FindName("textBox" + i.ToString()));
             }
         }
 
@@ -65,9 +67,12 @@ namespace CountDownApp
         */
         private void sendCharToTextBox(char _letter)
         {
-            _textboxeArray[_textBoxLoc].Text = _letter.ToString();
 
-            if (_textBoxLoc == _textboxeArray.Length-1)
+            _listAvailLetters.Add(_letter);//Add the current letters to a list 
+            _textboxArray[_textBoxLoc].Text = _letter.ToString();//send the textbox the letter
+
+            //if all letters are added hide buttons
+            if (_textBoxLoc == _textboxArray.Length-1)
             {
                 _textBoxLoc = 0;
                 btnVowels.Visibility = Visibility.Collapsed;
@@ -77,6 +82,8 @@ namespace CountDownApp
             }         
             else
                 _textBoxLoc++;
+
+            //START TIMER HERE
         }
 
         /*
@@ -91,16 +98,55 @@ namespace CountDownApp
 
         private void btnCheckWord_Click(object sender, RoutedEventArgs e)
         {
-            //Check that the Word is using the same letters Displayed
+            chkLettersValidity();
 
 
-            validateUserWord();
 
 
+            //RESET AFTER THE GAMES OVER
             resetGame();
         }
 
-        private void validateUserWord()
+        //Check that the letters entered by the user(converted to uppercase) are the letters that were displayed
+        //and that there are no duplicate letters used by the user when not available 
+        private void chkLettersValidity()
+        {
+            Boolean _validLetters = true;
+            String _toUpper;
+            int _leterIndex;
+            //convert the users input to upper case
+            _toUpper = txtBoxUsrWord.Text.ToUpper();
+
+            //creates a new char array from the string to check for correct letters
+            Char[] newArray = _toUpper.ToCharArray();
+
+            //Check that the Word is using the same letters Displayed
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                //Finds the index of the current letter [i]  in the list _listAvailLetters, 
+                _leterIndex = _listAvailLetters.IndexOf(newArray[i]);
+
+                //If the letters not in the list
+                if (_leterIndex == -1)
+                {
+                    _validLetters = false;
+                }
+                //Remove the letter from the list of available letters to stop duplicate letters 
+                else
+                    _listAvailLetters.RemoveAt(_leterIndex);
+            }
+
+            if (_validLetters == true)
+            {
+                //Letters are valid 
+                chkWordValidity();
+            }
+            else {
+                //Letters are NOT valid 
+            }
+        }
+
+        private void chkWordValidity()
         {
             //Searches the _wordsList for the word entered by the user and gives its index
             int i = App._wordsList.BinarySearch(txtBoxUsrWord.Text);
@@ -113,6 +159,10 @@ namespace CountDownApp
 
             //IF THE WORD IS CORRECT, GET THE "WORDS LENGTH" AND ADD THAT TO THERE SCORE
         }
+
+
+
+
 
         /*
         Resets the games: Buttons, and emptys the textboxes containing letters
@@ -127,8 +177,9 @@ namespace CountDownApp
             btnCheckWord.Visibility = Visibility.Collapsed;
 
             //Reset all the textboxes to have nothing in them
-            foreach (TextBox t in _textboxeArray) {
-                _textboxeArray[_textBoxLoc++].Text = "";
+            foreach (TextBox t in _textboxArray) {
+                _textboxArray[_textBoxLoc++].Text = "";
+                _listAvailLetters.Clear();//Clear the list
             }
             _textBoxLoc = 0;
         }
